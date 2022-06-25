ff6dc168a0f2abf1f1373a34569c3e27ab50bb19
EraldoAbedini <abedinieraldo@gmail.com>
2022-06-25 17:17:37 +0200

    added object user

--- src/main/java/spring/postgres/app/spring/project/Application.java
+++ src/main/java/spring/postgres/app/spring/project/Application.java
@@ -1,15 +1,27 @@
 package spring.postgres.app.spring.project;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+import spring.postgres.app.spring.project.entities.User;
+import spring.postgres.app.spring.project.services.SpidService;
+import spring.postgres.app.spring.project.services.UserService;
 
 @SpringBootApplication
 @RestController
-public class Application {
+public class Application implements CommandLineRunner {
+    @Autowired
+    UserService userService;
+    SpidService spidService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(userService.createUser(new User("Eraldo", "Abedin", "..", "Eraldo_Abedin", "lol", "abedinieraldo@gmail.com")));
+    }
 }
